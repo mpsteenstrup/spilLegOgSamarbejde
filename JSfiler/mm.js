8bfd670f1b87e8dd0669f3697228019488a532c3
mpsteenstrup <mpsteenstrup@gmail.com>
2020-08-12 12:28:19 +0200

    first

---
+++ JSfiler/mm.js
@@ -0,0 +1,34 @@
+function setup() {
+createCanvas(windowWidth*0.9, windowHeight*0.2);
+t1=0; t2=0; n = 0; score=0;
+}
+
+function draw() {
+
+	background(0);
+
+	if (n==0){
+			fill(255,0,0);
+			ellipse(500,500,200,200);
+	}
+
+	timeIngervalRandomNumber();
+}
+
+
+function keyPressed(){
+	if (keyCode == UP_ARROW  && n==0)
+	{
+		score += 1;
+		n=int(random(4));
+		print(score);
+	}
+}
+
+function timeIngervalRandomNumber() {
+	t2=millis();
+	if (t2-t1>1000){
+		t1=t2;
+		n=int(random(4));
+	}
+}
